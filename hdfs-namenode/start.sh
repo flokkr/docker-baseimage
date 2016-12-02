9ab90a4f0f1c0478afa2e3f657ede5425f64312c
Elek, Márton <elek@users.noreply.github.com>
2016-12-02 10:24:11 +0100

    move out bigdata-base from separated repository

--- hdfs-namenode/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-set -x
-source /opt/hadoop-configurator.sh
-export CONF_DIR=/opt/hadoop/etc/hadoop
-initialize
-configure core-site
-configure hdfs-site
-/opt/hadoop/bin/hdfs namenode
