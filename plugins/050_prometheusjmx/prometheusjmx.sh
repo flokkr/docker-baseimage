79d5e3f1ff3cf7e50e92038a6d2e6fddce47e9bf
Elek, Márton <elek@users.noreply.github.com>
2017-11-09 22:31:11 +0100

    switch to jmxpromo from jmx_exporter

--- plugins/050_prometheusjmx/prometheusjmx.sh
+++ plugins/050_prometheusjmx/prometheusjmx.sh
@@ -3,19 +3,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [ "true" == "$PROMETHEUSJMX_ENABLED" ]; then
   plugin-is-active "Prometheus JMX exporter"
-  EXPORTER_FILE="/tmp/jmx_prometheus_javaagent-0.11-SNAPSHOT.jar"
+  EXPORTER_FILE="/tmp/jmxpromo.jar"
   if [ ! -f "$EXPORTER_FILE" ]; then
-     wget https://kv.anzix.net/jmx_prometheus_javaagent-0.11-SNAPSHOT.jar -O $EXPORTER_FILE
+     wget https://kv.anzix.net/jmxpromo.jar -O $EXPORTER_FILE
   fi
-  export AGENT_STRING="-javaagent:$EXPORTER_FILE=0:$CONF_DIR/config.yaml"
-  if [ ! -f "$CONF_DIR/config.yaml" ]; then
-     cat << EOF > $CONF_DIR/config.yaml
----
-startDelaySeconds: 0
-EOF
-  fi
-  declare -x $JAVA_OPTS_VAR="$AGENT_STRING ${!JAVA_OPTS_VAR}"
-  echo "Process is instrumented with setting $AGENT_STRING"
-fi
+  export AGENT_STRING="-javaagent:$EXPORTER_FILE=$PROMETHEUSJMX_AGENTOPTS"
 
 call-next-plugin "$@"
